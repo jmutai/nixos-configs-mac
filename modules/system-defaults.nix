b1125661ad2156602ee3e966f120d1e1936bd001
jmutai <josphatkmutai@gmail.com>
2025-11-07 19:57:20 +0300

    restore  function keys

--- modules/system-defaults.nix
+++ modules/system-defaults.nix
@@ -44,7 +44,7 @@
       NSGlobalDomain = {
         "com.apple.swipescrolldirection" = true; # enable natural scrolling(default to true)
         "com.apple.sound.beep.feedback" = 0;     # disable beep sound when pressing volume up/down key
-        "com.apple.keyboard.fnState" = true;     # fn for f1-12
+        "com.apple.keyboard.fnState" = false;    # Use media keys by default
         AppleInterfaceStyle = "Dark";            # dark mode
 
         # Keyboard settings
